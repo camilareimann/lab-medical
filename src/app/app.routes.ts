1b94505bd6fe855aa53db83432b1ede7909db049
Camila Reimann <56306933+camilareimann@users.noreply.github.com>
2024-04-10 18:31:37 -0300

    criada pagina home com integração de sidebar e toolbar. necessitou algumas adaptações no toolbar para que ele se adapte quando o sidebar está fechado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,18 @@ import { Routes } from '@angular/router';
 import { SinginSignupPageComponent } from './singin-signup-page/singin-signup-page.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    
     {
-        path: "",
-        component: AppComponent
+        path: "", 
+        redirectTo: "home", 
+        pathMatch: "full" 
+    },
+    {
+        path: "home",
+        component: HomeComponent
     },
     {
         path: "login",
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent, ToolbarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+  isMenuRetracted = false;
+
+  onSidebarRetracted(isRetracted: boolean) {
+    this.isMenuRetracted = isRetracted;
+  }
+
+}
