daed710cf1919e5264a51c3e89039bf8c815f2be
Camila Reimann <56306933+camilareimann@users.noreply.github.com>
2024-04-09 21:58:50 -0300

    implementacao de logica de login e cadastro com local storage e auth service + página de login e cadastro conjunta com animação em css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { SinginSignupPageComponent } from './sing-in-sign-up/singin-signup-page.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: SinginSignupPageComponent
+    },
+
+    {
+        path: "cadastro",
+        component: SignUpComponent
+    },
+
+];
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private router: Router) { }
+
+
+  onSubmit(loginForm: FormGroup) {
+    const storedData = JSON.parse(localStorage.getItem('cadastroData') || '[]');
+    const email = loginForm.get('email')?.value;
+    const password = loginForm.get('password')?.value;
+  
+    const user = storedData.find((userData: any) => userData.email === email && userData.senha === password);
+  
+    if (user != undefined) {
+
+      localStorage.setItem('loggedUser', JSON.stringify(user))
+
+      alert('logged')
+      
+      // this.router.navigate(['/home']); //, { state: { userData: user } }
+      
+    } else {
+      alert('Usuário ou senha inválidos');
+    }
+  }
+
+  
+
+  esqueciSenha(loginForm: FormGroup) {
+    const storedData = JSON.parse(localStorage.getItem('cadastroData') || '[]');
+    const email = loginForm.get('email')?.value;
+  
+    const userIndex = storedData.findIndex((userData: any) => userData.email === email);
+  
+    if (userIndex !== -1) {
+      storedData[userIndex].senha = 'a1b2c4d4';
+      localStorage.setItem('cadastroData', JSON.stringify(storedData));
+      alert('Sua senha foi alterada para a senha padrão: a1b2c4d4. Por favor, prossiga utilizando essa senha.');
+    } else {
+      alert('Usuário não encontrado');
+    }
+  }
+
+  // logout() {
+  //   localStorage.removeItem('loggedUser');
+  //   this.router.navigate(['/login']);
+  // }
+
+}
