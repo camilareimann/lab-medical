6a8dc6b0339c3bc3f2e3baa61765a3a72827a7cd
Camila Reimann <56306933+camilareimann@users.noreply.github.com>
2024-04-12 15:29:49 -0300

    integração de card com a home para exibicao de dados dos pacientes cadastrados

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,15 +4,24 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule } from '@angular/common';
+import { PacienteService } from '../services/paciente.service';
+import { CardComponent } from '../card/card.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, ToolbarComponent, FontAwesomeModule, CommonModule],
+  imports: [RouterOutlet, SidebarComponent, ToolbarComponent, FontAwesomeModule, CommonModule, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  pacienteData: any[] = [];
+
+  constructor(private pacienteService: PacienteService){
+    this.pacienteData = this.pacienteService.getAllPatients();
+  }
 
   isMenuRetracted = false;
   pageTitle: string = 'Página Inicial';
