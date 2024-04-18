5fcb5649e1f8fddf69080d2651c725ad8fd66eb3
Camila Reimann <56306933+camilareimann@users.noreply.github.com>
2024-04-18 18:17:17 -0300

    Revert "add metodo para exibir pacientes e exames/consultas  em ordem alfabet…"

--- src/app/services/paciente.service.ts
+++ src/app/services/paciente.service.ts
@@ -17,7 +17,7 @@ export class PacienteService {
   }
 
   getAllPatients(): any[] {
-    return this.pacienteList.slice().sort((a, b) => a.name.localeCompare(b.name));
+    return this.pacienteList;
   }
 
   getPatientById(patientId: string): any {
@@ -50,6 +50,11 @@ export class PacienteService {
     }
   }
 
+  private saveToLocalStorage(): void {
+    localStorage.setItem('pacienteData', JSON.stringify(this.pacienteList));
+  }
+
+
   addExam(patientId: string, exam: any): void {   
     const patient = this.getPatientById(patientId);
     
@@ -60,7 +65,6 @@ export class PacienteService {
         const examId = uuidv4();
         exam.id = examId;
         patient.exams.push(exam);
-        patient.exams.sort((a: any, b: any) => new Date(a.date).getTime() - new Date(b.date).getTime());
         this.saveToLocalStorage();
     }
 }
@@ -71,7 +75,6 @@ export class PacienteService {
       const index = patient.exams.findIndex((e: any) => e.id === updatedExam.id);
       if (index !== -1) {
         patient.exams[index] = { ...updatedExam };
-        patient.exams.sort((a: any, b: any) => new Date(a.date).getTime() - new Date(b.date).getTime());
         this.saveToLocalStorage();
       }
     }
@@ -97,7 +100,6 @@ export class PacienteService {
       const consultaId = uuidv4();
       consulta.id = consultaId;
       patient.consultas.push(consulta);
-      patient.consultas.sort((a: any, b: any) => new Date(a.date).getTime() - new Date(b.date).getTime());
       this.saveToLocalStorage();
     }
   }
@@ -108,7 +110,6 @@ export class PacienteService {
       const index = patient.consultas.findIndex((c: any) => c.id === updatedConsulta.id);
       if (index !== -1) {
         patient.consultas[index] = { ...updatedConsulta };
-        patient.consultas.sort((a: any, b: any) => new Date(a.date).getTime() - new Date(b.date).getTime());
         this.saveToLocalStorage();
       }
     }
@@ -124,10 +125,5 @@ export class PacienteService {
       }
     }
   }
-
-  private saveToLocalStorage(): void {
-    localStorage.setItem('pacienteData', JSON.stringify(this.pacienteList));
-  }
-
 
 }
